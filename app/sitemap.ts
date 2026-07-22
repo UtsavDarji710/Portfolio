7248807cd789636a91a4f2badd1a652ec557bb3b
Utsavdarji <71565716+UtsavDarji710@users.noreply.github.com>
2026-07-22 11:08:33 +0530

    upda: Change sitemap change frequency from monthly to daily

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: siteConfig.url,
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "daily",
       priority: 1,
     },
   ];
